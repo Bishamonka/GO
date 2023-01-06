3778d9a2e55327304f3c24c154ec2541c27a9f22
O_ <104556158+Bishamonka@users.noreply.github.com>
2023-01-06 14:33:18 +0200

    Add files via upload

---
+++ G1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	fmt.Println(">>> There is not much of a code...")
+	time.Sleep(2 * time.Second)
+	fmt.Println(">>> But that certain feeling that you've built something from scratch in another cool language is priceless.")
+	time.Sleep(2 * time.Second)
+	fmt.Println(">>> That there is a file, that you simply double click and it works...")
+	time.Sleep(2 * time.Second)
+	fmt.Println(">>> Feels good.")
+	time.Sleep(2 * time.Second)
+
+}
